55c346968baa435306664b53a41550e95ee94e83
Andrew <andrew.mk4@yahoo.com>
2024-06-14 01:51:30 -0700

    program cards complete

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Programs.css";
 import {programsData} from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
 
 
 const Programs = () => {
@@ -12,11 +13,17 @@ const Programs = () => {
                 <span className="stroke-text">to shape you</span>
             </div>
             
-            <div className="program-catergories">
+            <div className="program-categories">
                 {programsData.map((program) => (
                     <div className="category">
-                        {program.img}
-                        <span>{program.heading}</span><span>{program.details}</span>
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+                        <div className="join-now">
+                            <span>Join Now</span>
+                            <img src={RightArrow} alt=""  
+                            />
+                        </div>
                     </div>
                 ))}
             </div>
