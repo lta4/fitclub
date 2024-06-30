e2751c7013121ec97725101aa0ca2207c3e47023
Andrew <andrew.mk4@yahoo.com>
2024-06-30 04:29:35 -0700

    test img ani added

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,11 +3,13 @@ import "./Testimonials.css";
 import { testimonialsData } from "../../data/testimonialsData";
 import leftArrow from "../../assets/leftArrow.png";
 import rightArrow from "../../assets/rightArrow.png";
+import {motion} from "framer-motion";
 
 const Testimonials = () => {
-
+    const transition = { type: "spring", duration: 3 };
     const [selected, setSelected] = useState(0);
     const tLength = testimonialsData.length;
+
     return (
         <div className="Testimonials">
             <div className="left-t">
@@ -25,8 +27,16 @@ const Testimonials = () => {
                 </span>
             </div>
             <div className="right-t">
-                <div></div>
-                <div></div>
+                <motion.div 
+                initial={{opacity: 0, x: -100}}
+                whileInView={{opacity: 1, x: 0}}
+                transition={{...transition, duration: 2}}
+                ></motion.div>
+                <motion.div 
+                initial={{opacity: 0, x: 100}}
+                whileInView={{opacity: 1, x: 0}}
+                transition={{...transition, duration: 2}}
+                ></motion.div>
                 <img src={testimonialsData[selected].image} alt="" />
                 <div className="arrows">
                     <img 
