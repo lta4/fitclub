9beefeb00b286bd25d744b50650581c4f608093b
Andrew <andrew.mk4@yahoo.com>
2024-07-05 05:32:45 -0700

    hamburger made

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png"
+import Bars from "../../assets/bars.png";
 
 const Header = () => {
+
+    const mobile = window.innerWidth <= 768 ? true: false;
+
+    const [menuOpened, setMenuOpened] = useState(false);
+
     return (
         <div className="header">
             <img src={Logo} alt="" className="logo" />
-            <ul className="header-menu">
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why us</li>
-                <li>Plans</li>
-                <li>Testimonials</li>
-            </ul>
+            {menuOpened === false && mobile === true ? (
+                <div>
+                    <img src={Bars} alt="" />
+                </div>
+            ) : (
+                <ul className="header-menu">
+                    <li>Home</li>
+                    <li>Programs</li>
+                    <li>Why us</li>
+                    <li>Plans</li>
+                    <li>Testimonials</li>
+                </ul>
+            )}
         </div>
     );
 };
