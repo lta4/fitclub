e98dd5beab5feed8e7dd5b9aa701d8af4588ed30
Andrew <andrew.mk4@yahoo.com>
2024-07-08 13:29:26 -0700

    onclick links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,16 +13,24 @@ const Header = () => {
         <div className="header">
             <img src={Logo} alt="" className="logo" />
             { menuOpened === false && mobile === true ? (
-                <div>
+                <div
+                style={{
+                backgroundColor: "var(--appColor)", 
+                padding: "0.5rem",
+                borderRadius: "5px",
+                }}
+
+                onClick={() => setMenuOpened(true)}
+                >
                     <img src={Bars} alt="" style={{width: '1.5rem', height: '1.5rem'}} />
                 </div>
             ) : (
                 <ul className="header-menu">
-                    <li>Home</li>
-                    <li>Programs</li>
-                    <li>Why us</li>
-                    <li>Plans</li>
-                    <li>Testimonials</li>
+                    <li onClick={() => setMenuOpened(false)}>Home</li>
+                    <li onClick={() => setMenuOpened(false)}>Programs</li>
+                    <li onClick={() => setMenuOpened(false)}>Why us</li>
+                    <li onClick={() => setMenuOpened(false)}>Plans</li>
+                    <li onClick={() => setMenuOpened(false)}>Testimonials</li>
                 </ul>
             )}
         </div>
